d4efd8b631b61886c4d7885f120de715fb2c73e1
Wesley Tejeda <xuene07@gmail.com>
2020-06-15 23:11:59 -0400

    Removed filler text from index html, Updated JS file with an extra ajax call

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 
 
-
+// my key: 4973b465133a426fc92080785c763347
 var apiKey = "4973b465133a426fc92080785c763347";
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Philadelphia&appid="+apiKey;
-
+var longitude = 0;
+var latitude = 0;
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function(response){
-    console.log(response);
+}).then(function(weatherData){
     //What we need:
-    //Temp, Humidity, Wind Speed, UV index, Temp ICON
+    //Grab longitude and longitude from city
+    console.log(weatherData);
+    longitude = weatherData.city.coord.lon;
+    lattitude = weatherData.city.coord.lat;
+
+            //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
+    queryURL="https://api.openweathermap.org/data/2.5/onecall?lat="+lattitude+"&lon="+longitude+"&appid="+apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(newWeatherData){
+        console.log(lattitude+" "+longitude);
+        console.log(newWeatherData);
+        var temp = ((newWeatherData.daily[0].temp.day) - 273.15) * 9/5 + 32;
+        var humid = newWeatherData.daily[0].humidity;
+        var windSpd = newWeatherData.daily[0].wind_speed;
+        var uvIndex = newWeatherData.daily[0].uvi;
+        console.log(temp+"|"+humid+"|"+windSpd+"|"+uvIndex);
+    });
 });
